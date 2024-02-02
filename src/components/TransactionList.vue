d95630218f2085a6d94770bc9683612364a3976b
Rasik Abdullah <rasikabdullah@gmail.com>
2024-02-03 02:24:10 +0600

    update

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,11 +1,29 @@
 <template>
-    <h3>History</h3>
-      <ul id="list" class="list">
-        <li class="minus">
+  <h3>History</h3>
+  <ul id="list" class="list">
+    <li
+      v-for="transaction in transactions"
+      :key="transaction.id"
+      :class="transaction.amount < 0 ? 'minus' : 'plus'"
+    >
+      {{ transaction.text }} <span>${{ transaction.amount }}</span
+      ><button class="delete-btn">x</button>
+    </li>
+    <!-- <li class="minus">
           Cash <span>-$400</span><button class="delete-btn">x</button>
         </li>
         <li class="plus">
           Paycheck <span>-$400</span><button class="delete-btn">x</button>
-        </li>
-      </ul>
+        </li> -->
+  </ul>
 </template>
+
+<script setup>
+    import { defineProps } from 'vue';
+    const props = defineProps({
+        transactions:{
+            type: Array,
+             required: true,
+        }
+    })
+</script>
