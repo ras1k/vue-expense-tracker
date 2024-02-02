3de0dcd18a18cdeaa67aebde4cd981660dafa973
Rasik Abdullah <rasikabdullah@gmail.com>
2024-02-03 01:44:42 +0600

    added transaction

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
     <Balance />
     <IncomeExpenses />
     <TransactionList />
+    <AddTransaction />
   </div>
 </template>
 
@@ -12,6 +13,7 @@ import Header from "./components/Header.vue";
 import Balance from "./components/Balance.vue";
 import IncomeExpenses from "./components/IncomeExpenses.vue";
 import TransactionList from "./components/TransactionList.vue";
+import AddTransaction from "./components/AddTransaction.vue";
 
 export default {
   components: {
@@ -19,6 +21,7 @@ export default {
     Balance,
     IncomeExpenses,
     TransactionList,
+    AddTransaction,
   },
 };
 </script>
